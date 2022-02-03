3ab6ea8add23102a5cb0402f017d427f4d6d9149
breannemcalli <breanne.mcallister@flemingcollege.ca>
2022-02-02 20:58:38 -0500

    test

--- story/netherlandjson.js
+++ story/netherlandjson.js
@@ -1,1 +1,19 @@
+function initMap(){
+const map = new google.maps.Map(document.getElementById("map"),{
+zoom: 4,
+center: { lat: 52.3676, lng: 4.9041 },
+});
+const labels = "123456789";
 
+const markers = locations.map((location, i) =>{
+    return new google.maps.Marker({
+        position:location,
+        label: labels[i % labels.length],
+    });
+});
+}
+const locations = [
+
+    { lat:52.35860205454664, lng: 4.881079338206324},
+
+]
