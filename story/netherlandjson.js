9f54e47d2fd64c7bb462d8d4149049e686f16e29
breannemcalli <breanne.mcallister@flemingcollege.ca>
2022-02-02 21:08:46 -0500

    test

--- story/netherlandjson.js
+++ story/netherlandjson.js
@@ -1,26 +1,13 @@
 function initMap() {
+    const myLatLng = { lat:52.3957590338986, lng:4.929824658990868};
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 3,
-      center: { lat: 51.853018, lng: -86.587375 },
+      zoom: 4,
+      center: myLatLng,
     });
-    // Create an array of alphabetical characters used to label the markers.
-    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    // Add some markers to the map.
-    // Note: The code uses the JavaScript Array.prototype.map() method to
-    // create an array of markers based on a given "locations" array.
-    // The map() method here has nothing to do with the Google Maps API.
-    const markers = locations.map((location, i) => {
-      return new google.maps.Marker({
-        position: location,
-        label: labels[i % labels.length],
-      });
-    });
-    // Add a marker clusterer to manage the markers.
-    new MarkerClusterer(map, markers, {
-      imagePath:
-        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  
+    new google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: "Hello World!",
     });
   }
-  const locations = [
-      {lat:52.34440245337607, lng:4.872833084332331}
-  ]
